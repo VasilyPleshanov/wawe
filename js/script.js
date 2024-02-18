60fec47cc39d270dab191a73e4f8d30dc7e5da26
Vasily Pleshanov <vasilypleshanov@gmail.com>
2024-02-18 10:59:45 +0300

    added video block

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ const burger = document.querySelector('.header__burger')
 const menu = document.querySelector('.header__menu')
 const body = document.querySelector('body')
 const header = document.querySelector('.header')
-// const headerContainer = document.querySelector('.header__container')
 
+const videoItem = document.querySelector('.video__item')
+const video = document.querySelector('#video')
+const videoBtn = document.querySelector('.video__btn')
+
+// Анимация меню бургер
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     menu.classList.toggle('active')
@@ -11,11 +15,27 @@ burger.addEventListener('click', () => {
     header.classList.toggle('active')
 })
 
+// Анимация header при начале скролла вниз
 window.addEventListener('scroll', function(event) {
     if (scrollY > 50) {
         header.classList.add('show')
     } else if (scrollY < 50) {
         header.classList.remove('show')
     }
-    
 })
+
+// Видео
+videoBtn.addEventListener('click', () => {
+    if (videoItem.classList.contains('play')) {
+        videoItem.classList.remove('play')
+        video.pause()
+    } else {
+        videoItem.classList.add('play')
+        video.play()
+    }
+})
+
+video.addEventListener('pause', () => {
+    videoItem.classList.remove('play')
+})
+
